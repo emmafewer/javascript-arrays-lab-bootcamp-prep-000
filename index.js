787386dfb02e4778f4f06fd3af41c50c4aadbf77
Emma Fewer <emmafewer20@gmail.com>
2020-09-15 15:51:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,3 +23,11 @@ function appendKitten(name) {
 function prependKitten(name) {
   return ["Arnold", ...kittens]
 }
+
+function removeLastKitten(name) {
+  return []
+}
+
+function removeFirstKitten(name) {
+  return kittens.slice
+}
